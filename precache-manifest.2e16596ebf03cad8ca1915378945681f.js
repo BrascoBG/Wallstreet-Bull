140429c1054b7ec7ec4304c919ff359014f22f13
Ivo Krastev <ivo.krustev@yahoo.com>
2020-08-10 13:13:07 +0300

    Updates

---
+++ precache-manifest.2e16596ebf03cad8ca1915378945681f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0445456d8954ab2ad33b609fec64eed7",
+    "url": "/Wallstreet-Bull/index.html"
+  },
+  {
+    "revision": "f6ba94131f2789f1101f",
+    "url": "/Wallstreet-Bull/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6b1420445e8ef24cb956",
+    "url": "/Wallstreet-Bull/static/css/main.acd542e1.chunk.css"
+  },
+  {
+    "revision": "f6ba94131f2789f1101f",
+    "url": "/Wallstreet-Bull/static/js/2.52871e34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Wallstreet-Bull/static/js/2.52871e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1420445e8ef24cb956",
+    "url": "/Wallstreet-Bull/static/js/main.e18ed06f.chunk.js"
+  },
+  {
+    "revision": "fb0020b0a0595d80b5a2",
+    "url": "/Wallstreet-Bull/static/js/runtime-main.8a71acb3.js"
+  },
+  {
+    "revision": "5f4336b37dc1236b7fcbc73b3e5a3d88",
+    "url": "/Wallstreet-Bull/static/media/bg.5f4336b3.jpg"
+  },
+  {
+    "revision": "26502b6caa77a063a3437ac4f6e60594",
+    "url": "/Wallstreet-Bull/static/media/bull.26502b6c.png"
+  },
+  {
+    "revision": "030c3347bf92deeef088ef75f68e7fed",
+    "url": "/Wallstreet-Bull/static/media/msft.030c3347.png"
+  },
+  {
+    "revision": "6d97cf32aa6c6c83ec493586af7e80d1",
+    "url": "/Wallstreet-Bull/static/media/wall.6d97cf32.jpg"
+  },
+  {
+    "revision": "8b57ad86daeddbf77e47c90f019b9e78",
+    "url": "/Wallstreet-Bull/static/media/wallstreet.8b57ad86.jpg"
+  },
+  {
+    "revision": "dfb3ac235f4293442537c16781b3f034",
+    "url": "/Wallstreet-Bull/static/media/wallstreet2.dfb3ac23.jpg"
+  }
+]);
